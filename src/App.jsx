a085df75fad62c3550c7bb8b1ac6659fc146a862
MCiccone <cicconemt@gmail.com>
2026-01-29 15:47:53 -0800

    Finished Lab

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,156 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [team, setTeam] = useState([])
+  const [money, setMoney] = useState(100)
+  const [zombieFighters, setZombieFighters] = useState(
+    [
+      {
+        id: 1,
+        name: 'Survivor',
+        price: 12,
+        strength: 6,
+        agility: 4,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png',
+      },
+      {
+        id: 2,
+        name: 'Scavenger',
+        price: 10,
+        strength: 5,
+        agility: 5,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png',
+      },
+      {
+        id: 3,
+        name: 'Shadow',
+        price: 18,
+        strength: 7,
+        agility: 8,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png',
+      },
+      {
+        id: 4,
+        name: 'Tracker',
+        price: 14,
+        strength: 7,
+        agility: 6,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png',
+      },
+      {
+        id: 5,
+        name: 'Sharpshooter',
+        price: 20,
+        strength: 6,
+        agility: 8,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png',
+      },
+      {
+        id: 6,
+        name: 'Medic',
+        price: 15,
+        strength: 5,
+        agility: 7,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png',
+      },
+      {
+        id: 7,
+        name: 'Engineer',
+        price: 16,
+        strength: 6,
+        agility: 5,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png',
+      },
+      {
+        id: 8,
+        name: 'Brawler',
+        price: 11,
+        strength: 8,
+        agility: 3,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png',
+      },
+      {
+        id: 9,
+        name: 'Infiltrator',
+        price: 17,
+        strength: 5,
+        agility: 9,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png',
+      },
+      {
+        id: 10,
+        name: 'Leader',
+        price: 22,
+        strength: 7,
+        agility: 6,
+        img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png',
+      },
+    ]
+  );
+  const handleAddFighter = (fighter) => {
+    setTeam([...team, fighter])
+    setMoney(money - fighter.price)    
+    const index = zombieFighters.findIndex(f => f.id === fighter.id)
+    zombieFighters.splice(index, 1)  
+   }
+   const handleRemoveFigther = (fighter) => {
+    setZombieFighters([...zombieFighters, fighter])
+    setMoney(money + fighter.price)
+    const index = team.findIndex(f => f.id === fighter.id)
+    team.splice(index, 1)
+   }
+   let totalStrength = team.reduce((total, fighter) => {
+    return total + fighter.strength;
+   }, 0);
+   let totalAgility = team.reduce((total, fighter) => {
+    return total + fighter.agility;
+   }, 0);
+  
 
   return (
     <>
-      <h1>hello world</h1>
+    <h1>Zombie Fighters</h1>
+    <div>
+      Money: {money}
+    </div>
+    <div>
+      Total Strength: {totalStrength}
+    </div>
+    <div>
+      Total Agility: {totalAgility}
+    </div>
+    <div>
+      <h3>Team</h3>
+      <ul>
+        {team.map((fighter, index) => (
+          <li key={index}>
+            <img src={fighter.img} alt={fighter.name} />
+            <p>{fighter.name}</p>
+            <p>Price: {fighter.price}</p>
+            <p>Strenth: {fighter.strength}</p>
+            <p>Agility: {fighter.agility}</p>
+            <button onClick={() => handleRemoveFigther(fighter)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div>
+      <h3>List of Figthers</h3>
+      <ul>
+        {zombieFighters.map((fighter, index)=> (
+          <li key={index}>
+            <img src={fighter.img} alt={fighter.name} />
+            <p>{fighter.name}</p>
+            <p>Price: {fighter.price}</p>
+            <p>Strenth: {fighter.strength}</p>
+            <p>Agility: {fighter.agility}</p>
+            <button onClick={() => handleAddFighter(fighter)}>Add</button>
+          </li>
+
+        ))}
+
+      </ul>
+    </div>
 
     </>
   )
